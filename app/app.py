8f471b7422e059fe0e801492c1e17edb215b4ad0
kvbiii <kuba.koscian001@gmail.com>
2023-05-28 17:35:17 +0200

    Consultation css

--- app/app.py
+++ app/app.py
@@ -17,12 +17,8 @@ def index():
         results["Surname"] = request.form["Surname"]
         results["PESEL"] = request.form["PESEL"]
         results["Zip_code"] = request.form["Zip_Code"]
-        results["City"] = request.form["City"]
-        results["Street"] = request.form["Street"]
-        results["House_number"] = request.form["House_number"]
-        results["Information"] = request.form["Information"]
         print(results)
-        return redirect(url_for("consultation", result=results))
+        return redirect(url_for("", result=results))
 
     result = request.args.get("result")
     return render_template("consultation.html", result=result)
