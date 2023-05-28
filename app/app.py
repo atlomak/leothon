8d297ce2dba3f24ae46903a83ae898d710d81833
atlomak <andrzej.tlomak01@gmail.com>
2023-05-28 12:56:46 +0200

    simple working example

--- app/app.py
+++ app/app.py
@@ -4,17 +4,18 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+openai.api_type = "azure"
+openai.api_version = "2022-12-01"
+openai.api_base = "https://leothon4.openai.azure.com/"  # Your Azure OpenAI resource's endpoint value.
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            engine="TextDaVinci003",
+            prompt=generate_prompt(animal)
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
