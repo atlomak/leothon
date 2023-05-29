98b26cc246d70efd9f4248223406181c097b9a72
atlomak <andrzej.tlomak01@gmail.com>
2023-05-29 10:18:46 +0200

    audio recording 4 with openai

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, redirect, render_template, request, send_file, session
 import whisper
+from flask import Flask, render_template, request, send_file, session, redirect
+
 from .DocsGenerator import DocsGenerator
 from .gptintegrator import get_openai_response
 
@@ -20,7 +21,10 @@ def index():
     else:
         return render_template("login.html")
 
+
 app.secret_key = os.getenv("SECRET_KEY")
+
+
 @app.route("/login", methods=["POST"])
 def login():
     login = request.form["name"]
@@ -31,23 +35,48 @@ def login():
     else:
         return "Wrong login or password"
 
+
 @app.route("/logout", methods=["GET"])
 def logout():
     session.pop("user", None)
-    return render_template("login.html")
+    return redirect("/")
+
+
+# @app.route("/renderDocument", methods=["POST"])
+# def renderDocument():
+#     generator = DocsGenerator(static_data=get_static_data())
+#     patient_data = {
+#         "name": request.form["Name"],
+#         "surname": request.form["Surname"],
+#         "pesel": request.form["PESEL"],
+#         "address": request.form["Zip_Code"]
+#     }
+#     audio_desc = request.form["audio_desc"]
+#     docs = generator.generate_docs(patient_data=patient_data,
+#                                    gpt_response=get_openai_response(audio_desc))
+#
+#     return send_file(
+#         docs,
+#         as_attachment=True,
+#         attachment_filename='report.docx'
+#     )
+
 
 @app.route("/renderDocument", methods=["POST"])
-def renderDocument():
+def get_audio():
     generator = DocsGenerator(static_data=get_static_data())
+    file = request.files["audio"]
     patient_data = {
         "name": request.form["Name"],
         "surname": request.form["Surname"],
         "pesel": request.form["PESEL"],
         "address": request.form["Zip_Code"]
     }
-    audio_desc = request.form["audio_desc"]
+    with open("audio.wav", "wb") as f:
+        f.write(file.read())
+    audio_desc = get_transcription()
     docs = generator.generate_docs(patient_data=patient_data,
-                                   gpt_response=get_openai_response(get_transcription()))
+                                   gpt_response=get_openai_response(audio_desc))
 
     return send_file(
         docs,
@@ -56,30 +85,18 @@ def renderDocument():
     )
 
 
-@app.route("/upload_audio", methods=["POST"])
-def get_audio():
-    file = request.files["audio"]
-    patient_data = {
-        "name": request.form["firstName"],
-        "surname": request.form["lastName"],
-        "pesel": request.form["pesel"],
-    }
-    with open("audio.wav", "wb") as f:
-        f.write(file.read())
-    audio_desc = get_transcription()
-    return redirect("/consultation")
-
 def get_transcription():
     model = whisper.load_model("base")
     audio_desc = model.transcribe(audio="audio.wav", temperature=0.7, verbose=0, language="polish")["text"]
     return audio_desc
+
 
 def get_static_data():
     static_data = {
         "header": "Centrum Medycyny: Warszawa CM Atrium\n"
                   "Medicover Opieka Ambulatoryjna 010210201201\n"
                   "Poradnia medycyny pracy - 154",
-        "footer": "Lek.med Josh Giibun\n"
+        "footer": "Lek.med Jan Testowy\n"
                   "specjalista medycyny pracy\n"
                   "420692137"
     }
